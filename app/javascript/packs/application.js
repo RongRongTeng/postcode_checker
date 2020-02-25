a375a702abdef20afcfc3e4a5a5e0e187ceb123c
Ya-Rong, Teng <c00ooc0207@gmail.com>
2020-02-25 22:24:33 +0800

    feat: install dependencies

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,14 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("bootstrap")
+require("stylesheets/application.scss")
+require("admin-lte")
+require("@fortawesome/fontawesome-free/js/all")
 
+var jQuery = require("jquery")
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
